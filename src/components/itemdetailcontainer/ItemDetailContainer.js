c85e85adbadb9a68d1f76564f15791c23173a4bc
Florencia Amendola <amendolaflorencia@gmail.com>
2021-06-23 20:27:10 -0300

    Update ItemDetail

--- src/components/itemdetailcontainer/ItemDetailContainer.js
+++ src/components/itemdetailcontainer/ItemDetailContainer.js
@@ -2,29 +2,36 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import ItemDetail from "../itemdetail/ItemDetail";
 
-
 function ItemDetailContainer() {
-  const [dato, setData] = useState(null);
+  const [data, setData] = useState(null);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/1")
+    fetch("https://fakestoreapi.com/products?limit=5")
       .then((res) => res.json())
       .then((res) => {
         setData(res);
       });
   }, []);
-  console.log(dato);
+  console.log(data);
   return (
     <Container>
-      <ItemDetail
-        key={dato.id}
-        image={dato.image}
-        description={dato.description}
-        title={dato.title}
-        price={dato.price}
-        stock="9"
-      />
+      {data !== null ? (
+        data.map((dato, index) => {
+          return (
+            <ItemDetail
+              key={index}
+              image={dato.image}
+              description={dato.description}
+              title={dato.title}
+              price={dato.price}
+              stock="9"
+            />
+          );
+        })
+      ) : (
+        <p/>
+      )}
     </Container>
   );
 }
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
